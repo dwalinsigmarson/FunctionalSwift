0295fc701e67559843ec8f6d95f4bec6936596c0
Dmitry Davydenko <ddavidenko@mackiev.com>
2020-02-18 22:14:18 +0200

    Initial Functional List

---
+++ List/Sources/List/List.swift
@@ -0,0 +1,140 @@
+enum List<A> : Sequence {
+	
+	case end
+	indirect case node(head: A, tail: List<A>)
+	
+	init<T: Sequence>(_ sequence: T) where T.Element == A {
+		self = .end
+		sequence.reversed().forEach {
+			self = self.add($0)
+		}
+	}
+	
+	var top: A? {
+		switch self {
+			case .end:
+				return nil
+			case .node(let head, _):
+				return head
+		}
+	}
+	
+	var tail: List<A> {
+		switch self {
+			case .end:
+				return self
+			case .node(_, let tail):
+				return tail
+		}
+	}
+	
+	func makeIterator() -> ListIterator<A> {
+		return ListIterator(list: self)
+	}
+	
+	func add(_ a: A) -> List<A> {
+		.node(head: a, tail: self)
+	}
+	
+	func dropFirst(_ n: Int) -> List<A> {
+		switch self {
+			case .end:
+				return self
+			case .node(_, let tail):
+				return n - 1 > 0 ? tail.dropFirst(n - 1) : tail
+		}
+	}
+
+	func dropLast() -> List<A> {
+		switch self {
+			case .end:
+				return self
+			case .node(_, .end):
+				return .end
+			case .node(let n, let tail):
+				return .node(head: n, tail: tail.dropLast())
+		}
+	}
+	
+	func foldLeft<B>(_ b: B, f: (A, B) -> B) -> B {
+		switch self {
+		case .end:
+			  return b
+		case .node(let a, let tail):
+			  return tail.foldLeft(f(a, b), f: f)
+		}
+	}
+	
+	func foldRight<B>(_ b: B, f: (A, B) -> B) -> B {
+		return self.foldLeft(Self.end, f: Self.node).foldLeft(b, f: f)
+	}
+
+	func append(list:List<A>) -> List<A> {
+		return self.foldRight(list) { (elem, list) -> List<A> in
+			return .node(head: elem, tail: list)
+		}
+	}
+
+	func length() -> Int {
+		self.foldRight(0) { $1 + 1 }
+	}
+	
+	func copy() -> List<A> {
+		self.foldRight(Self.end, f: Self.node)
+	}
+	
+	func reversed() -> List<A> {
+		self.foldLeft(Self.end, f: Self.node)
+	}
+
+}
+
+struct ListIterator<A> : IteratorProtocol {
+	typealias Element = A
+	
+	var list: List<A>
+	
+	mutating func next() -> A? {
+		switch list {
+		case .end:
+      	return nil
+		case .node(let head, let tail):
+			list = tail
+			return head
+		}
+	}
+}
+
+extension List {
+	func foldRightExplicit<B>(_ b: B, f: (A, B) -> B) -> B {
+		switch self {
+		case .end:
+			  return b
+		case .node(let a, let tail):
+			  return f(a, tail.foldRight(b, f: f))
+		}
+	}
+	
+	func foldLeftViaFoldRifgt<B>(_ b: B,  f: @escaping (A, B) -> B) -> B {
+		self.foldRightExplicit({ $0 }) { (elem: A, accum: @escaping (B) -> B ) -> ((B) -> B) in
+			return { (b: B) -> B in
+				return accum(f(elem, b))
+			}
+		}(b)
+	}
+}
+
+extension List where A: AdditiveArithmetic {
+	func sum() -> A {
+		switch self {
+			case .end:
+				return .zero
+			case .node(let head, let tail):
+				return head + tail.sum()
+		}
+	}
+	
+	func sum2() -> A {
+		return self.foldRight(A.zero) { $0 + $1 }
+	}
+}
---
+++ List/Tests/ListTests/ListTests.swift
@@ -0,0 +1,77 @@
+import XCTest
+@testable import List
+
+final class ListTests: XCTestCase {
+	func testInitAddTop() {
+		let list0 = List<Int>([])
+		XCTAssertEqual(list0.top, nil, "List should be empty")
+		
+		let list1 = List<Int>([1])
+		XCTAssertEqual(list1.top, 1, "List should have one node with 1")
+
+		let list5 = List<Int>([1, 2, 3, 4, 5])
+		let list5Res = diffNumIn(list5, first: 1, last: 5)
+		XCTAssertNil(list5Res, "diff at num. \(list5Res!)")
+	}
+	
+	func testIteration() {
+		for _ in List<Int>([]) {
+			XCTFail()
+		}
+	
+		let list5 = List<Int>([1, 2, 3, 4, 5])
+		var i = 1
+		for elem in list5 {
+			XCTAssertEqual(i, elem, "diff at num \(i)")
+			i += 1
+		}
+	}
+
+	func testDropFirst() {
+		let list00 = List<Int>([]).dropFirst(0)
+		XCTAssertEqual(list00.top, nil, "List should be empty")
+
+		let list01 = List<Int>([]).dropFirst(1)
+		XCTAssertEqual(list01.top, nil, "List should be empty")
+
+//		let list10 = List<Int>([1]).dropFirst(0)
+//		XCTAssertEqual(list10.top, 1, "List should be empty")
+
+		let list11 = List<Int>([1]).dropFirst(1)
+		XCTAssertEqual(list11.top, nil, "List should be empty")
+
+		let listLong = List<Int>([1,2,3,4])
+		
+		let diffLong1 = diffNumIn(listLong.dropFirst(2), first: 3, last: 4)
+		XCTAssertNil(diffLong1, "diff at num \(diffLong1!)")
+		
+		let diffLong2 = diffNumIn(listLong.dropFirst(3), first: 4, last: 4)
+		XCTAssertNil(diffLong2, "diff at num \(diffLong2!)")
+
+		XCTAssertNil(listLong.dropFirst(4).top, "")
+	}
+
+	static var allTests = [
+		("testInit", testInitAddTop),
+		("testIteration", testIteration)
+	]
+}
+
+extension ListTests {
+	// TODO:
+	// XCTAssert... are not referential transparent, ha-ha-ha ))
+	// To rewrite, when I adopt functional error propagation ))
+	func diffNumIn(_ list: List<Int>, first: Int, last: Int) -> Int? {
+		func go(_ list: List<Int>, _ i: Int) -> Int? {
+			switch list {
+			case .end:
+				return i != last + 1 ? i : nil
+			case .node(let elem, let list):
+				return i > last || elem != i ?
+					i : go(list, i + 1)
+			}
+		}
+		
+		return go(list, first)
+	}
+}
